eb404b7240059a1f70e668c100cb8b0ba32c10df
IamMaheshGurung <gurungmahesh167@gmail.com>
2024-09-14 09:03:48 +0900

    Server and client

--- Basics/net_httpConcept/Server/main.go
+++ Basics/net_httpConcept/Server/main.go
@@ -1,22 +1,79 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain")
-	fmt.Fprintln(w, "Hi I am from the server")
+func main() {
+	router := http.NewServeMux()
+	home := http.HandlerFunc(handler)
+	form := http.HandlerFunc(formHandler)
+	router.Handle("/", home)
+	router.Handle("/form", form)
+
+	server := http.Server{
+		Addr:        ":8080",
+		Handler:     router,
+		IdleTimeout: 120 * time.Second,
+	}
+
+	go func() {
+		fmt.Println("Local host running at :8080")
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Printf("Server Error:%v", err)
+		}
+	}()
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt)
+	<-sig
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	println("Gracefully shutting down the server")
+
+	err := server.Shutdown(ctx)
+	if err != nil {
+		log.Printf("SHutdown due to %v", err)
+
+	}
+
 }
 
-func main() {
-	http.HandleFunc("/", handler)
-	fmt.Println("Server loading at local host")
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		log.Println("Unable to load the server")
+func handler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		_, err := fmt.Println("Hi Everyone I am the homepage")
+		if err != nil {
+			http.Error(w, "Unable to get the webpage", http.StatusBadRequest)
+		}
+
+	}
+	http.Error(w, "Unable to get the webpage", http.StatusBadRequest)
+
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		err := r.ParseForm()
+		if err != nil {
+			http.Error(w, "unable to parse form", http.StatusBadRequest)
+		}
+
+		name := r.FormValue("name")
+		email := r.FormValue("email")
+
+		response := fmt.Sprintf("Data Received %s as name and %s as email", name, email)
+		fmt.Fprintln(w, response)
+	} else {
+		http.Error(w, "Incorrect request method", http.StatusBadRequest)
 	}
 
 }
--- Basics/net_httpConcept/handler/main.go
+++ Basics/net_httpConcept/handler/main.go
@@ -3,29 +3,48 @@ package main
 import (
 	"fmt"
 	"io"
-
 	"net/http"
+	"net/url"
 )
 
 func main() {
-	url := "http://localhost:8080/"
+	formUrl := "http://localhost:8080/form"
 
-	resp, err := http.Get(url)
-
+	var name string
+	var email string
+	fmt.Print("Enter your name: ")
+	_, err := fmt.Scanf("%s\n", &name)
 	if err != nil {
-		fmt.Printf("Error making the request %v\n", err)
+		fmt.Println("Error reading name:", err)
 		return
 	}
 
+	fmt.Print("Enter your email: ")
+	_, err = fmt.Scanf("%s\n", &email)
+	if err != nil {
+		fmt.Println("Error reading email:", err)
+		return
+	}
+
+	formData := url.Values{
+		"name":  {name},
+		"email": {email},
+	}
+
+	resp, err := http.PostForm(formUrl, formData)
+
+	if err != nil {
+		fmt.Println("Error", err)
+		return
+	}
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Printf("Error in reading response body %v\n", err)
+		fmt.Println("Error", err)
 		return
 	}
 
-	fmt.Printf("Status Code %d\n", resp.StatusCode)
 	fmt.Printf("%s", body)
 
 }
