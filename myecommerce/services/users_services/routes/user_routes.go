1fbbf399619c57b48e05f893d11799fa7be3dd05
IamMaheshGurung <gurungmahesh167@gmail.com>
2024-11-27 07:48:03 +0900

    routers

--- myecommerce/services/users_services/routes/user_routes.go
+++ myecommerce/services/users_services/routes/user_routes.go
@@ -6,13 +6,20 @@ package routes
 import(
     "userService/handlers"
     "github.com/gorilla/mux"
+    "gorm.io/gorm"
+    
 )
 
 
 
-func SetupRoutes() * mux.Router {
-    router := mux.NewRouter()
+func SetupRoutes(db *gorm.DB) * mux.Router {
+    r := mux.NewRouter()
+    userService := handlers.NewUserService(db)
 
 
-
-    router.HandleFunc("/login", handler.NewUserService.
+    r.HandleFunc("/users", userService.CreateUser).Methods("POST")
+    r.HandleFunc("/users/{id}", userService.GetUser).Methods("GET")
+    r.HandleFunc("/users/{id}", userService.UpdateUser).Methods("PUT")
+    r.HandleFunc("/users/{id}", userService.DeleteUser).Methods("DELETE")
+return r
+}
